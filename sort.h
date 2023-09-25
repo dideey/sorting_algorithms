a9cdf7cc9dfff686afe8cda2a47dbcdf711f8023
dideey <bradleyjd32@gmail.com>
2023-09-25 19:31:04 +0300

    error check

--- sort.h
+++ sort.h
@@ -1,3 +1,4 @@
+
 #ifndef SORT_H
 #define SORT_H
 
@@ -13,9 +14,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+        const int n;
+        struct listint_s *prev;
+        struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -26,11 +27,10 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 
 
+
 void swap(int *a, int *b);
 void swap_node(listint_t **list, listint_t **a, listint_t *b);
 int count_list(listint_t **list);
-
-
 
 
 #endif
